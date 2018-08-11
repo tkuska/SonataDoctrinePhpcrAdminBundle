e3d006fe0b2e18ff9437e1f912c9b33876e4e1bf
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-08-11 07:21:35 +0200

    increase testing version

--- .travis/before_script_test.sh
+++ .travis/before_script_test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -ev
+TESTING_SCRIPTS_DIR=vendor/symfony-cmf/testing/bin
+CONSOLE=${TESTING_SCRIPTS_DIR}/console
 
-vendor/symfony-cmf/testing/bin/console doctrine:phpcr:init:dbal --drop --force
-vendor/symfony-cmf/testing/bin/console doctrine:phpcr:repository:init
+echo '+++ create PHPCR +++'
+${CONSOLE} doctrine:phpcr:init:dbal --drop --force
+${CONSOLE} doctrine:phpcr:repository:init
